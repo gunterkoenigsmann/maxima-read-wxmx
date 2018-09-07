7960ce169cd78cfd8ab39bed14c4dc57602bd6c0
Gunter Königsmann <gunter@peterpall.de>
2018-09-07 10:45:53 +0200

    A quick-lisp-less version that still is able to open any recent wxmx file.

--- maxima-read-wxmx.lisp
+++ maxima-read-wxmx.lisp
@@ -1,32 +1,53 @@
 ;; maxima_read_wxmx.lisp -- load Maxima code from wxmx file
 ;; copyright 2016 Robert Dodier
 ;; I release this work under terms of the GNU General Public License
+;; Quick-lisp-less version [uncompressed wxmx only] by Gunter Königsmann in 2018 
 
-(ql:quickload :zip)
-
-(defun $open_zipfile (file-name-zip)
-  (zip:open-zipfile file-name-zip))
-
-(defun $get_zipfile_entry (zipfile entry-name)
-  (zip:get-zipfile-entry entry-name zipfile))
-
-;; I WONDER IF THIS FUNCTION SHOULD JUST RETURN AN ARRAY.
-;; WHAT IF THE ENTRY CONTAINS AN IMAGE OR OTHER NON-TEXT FILE ??
-(defun $zipfile_entry_contents (zipfile-entry)
-  (coerce (mapcar #'code-char (coerce (zip:zipfile-entry-contents zipfile-entry) 'list)) 'string))
-
-(defun $get_wxmx_content_xml (file-name-wxmx)
-  ($zipfile_entry_contents ($get_zipfile_entry ($open_zipfile file-name-wxmx) "content.xml")))
-
-;; IN is an input stream, as created by make_string_input_stream or whatever
 
 (defun $load_stream (in)
   (catch 'macsyma-quit (continue in t)))
 
+;; Convert wxmx back to ASCII
+(defun wxmx-unquote-xml (x)
+  (let* ((tmp-x (string-substitute #\&        "&amp;" x))
+	 (tmp-x (string-substitute #\<        "&lt;"  tmp-x))
+	 (tmp-x (string-substitute #\>        "&gt;"  tmp-x))
+	 (tmp-x (string-substitute #\Return   "&#13;" tmp-x))
+	 (tmp-x (string-substitute #\Linefeed "&#13;" tmp-x))
+	 (tmp-x (string-substitute #\Newline  "&#13;" tmp-x)))))
+
 ;; returns a string containing the Maxima code from the content.xml entry in FILE-NAME-WXMX
-(defun $read_content_xml (file-name-wxmx)
-  (let ((s (xmls:parse (make-string-input-stream (mfuncall '$get_wxmx_content_xml file-name-wxmx)))))
-    (mfuncall '$join_lines (mfuncall '$extract_input (xml-to-expr s)))))
+;;
+;; Todo: An error message in the case that no<wxMaximaDocument and no </wxMaximaDocument>
+;; are found.
+(defun $read_content_xml (filename)
+  (with-open-file (stream filename)
+		  ;; Skip to the first line of the wxmx file's contents
+		  (loop for line = (read-line stream nil)
+			while (and
+			       line
+			       (not
+				(search "<wxMaximaDocument" line))))
+
+		  ;; Extract all lines containing a <line> and a </line> tag.
+		  (loop for line = (read-line stream nil)
+			while (and
+			       line
+			       (not
+				(search "</wxMaximaDocument>" line)))
+			do (
+			    let (
+				 (linestart (search "<line>"  line))
+				 (lineend   (search "</line>" line))
+				 )
+			     (if (and linestart lineend)
+				 (collect
+				  (wxmx-unquote-xml
+				   (subseq line
+					   (+ linestart 6)
+					   (- lineend 0)))))))))
+
+		  
 
 (defun $load_wxmx (file-name-wxmx)
   ($load_stream (make-string-input-stream ($read_content_xml file-name-wxmx))))
